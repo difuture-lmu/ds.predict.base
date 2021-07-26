9cf5227ee7c7cc5fdfdcc44b6636d0d574b0ec8b
schalkdaniel <d-schalk@t-online.de>
2021-07-26 15:06:33 +0200

    updates

---
+++ R/removeMissings.R
@@ -0,0 +1,15 @@
+#'
+#' @title Remove missing values from data
+#' @description Remove missing values from data and assign it to new data or old data.
+#' @param connections [DSI::connection] Connection to an OPAL server.
+#' @param symbol [character(1L)] R object from which missings should be removed.
+#' @param new_symbol [character(1L)] New R object to which the data is written.
+#' @author Daniel S.
+#' @export
+removeMissings = function(connections, symbol) {
+  if (missing(symbol)) stop("Symbol must be given.")
+  if (missing(new_symbol)) new_symbol = symbol
+
+  obj = eval(parse(text = symbol))
+  return(na.omit(obj))
+}
