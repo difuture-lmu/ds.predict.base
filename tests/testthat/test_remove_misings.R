8b2d4f63d77694c9d09b94b4348d5d27d423075d
schalkdaniel <d-schalk@t-online.de>
2021-07-27 10:14:30 +0200

    bigger update for system checks

--- tests/testthat/test_remove_misings.R
+++ tests/testthat/test_remove_misings.R
@@ -1,8 +1,8 @@
-context("Ceck if remove missings works correctly")
+context("Check if remove missings works correctly")
 
 test_that("remove missings really removes missings", {
 
-  x = data.frame(x = c(1, 2, 3), y = c(3, NA, 1))
+  x <<- data.frame(x = c(1, 2, 3), y = c(3, NA, 1))
   expect_error(removeMissings())
   expect_error(removeMissings("bla"))
   expect_equal(na.omit(x), removeMissings("x"))
