1c564381833e07f3b85d7c4fc32b1f2e74a51e86
schalkdaniel <d-schalk@t-online.de>
2022-03-01 09:28:17 +0100

    remove serialization separator to save memory

--- R/decodeBinary.R
+++ R/decodeBinary.R
@@ -2,7 +2,6 @@
 #' @title Deserialize object
 #' @description Decode a given string of a serialized object.
 #' @param bin (`character(1L)`) Binary string value containing the serialized model.
-#' @param sep (`character(1L)`) Separator used to collapse the binary elements (default is `-`).
 #' @param package (`character(1L)`) Package required for object deserialization (default is `NULL`).
 #' @return Deserialized object from `bin`
 #' @author Daniel S.
@@ -12,19 +11,18 @@
 #' mod_b = decodeBinary(bin)
 #' all.equal(mod, mod_b)
 #' @export
-decodeBinary = function(bin, sep = "-", package = NULL) {
+decodeBinary = function(bin, package = NULL) {
   checkmate::assertCharacter(bin, len = 1L, null.ok = FALSE, any.missing = FALSE)
-  checkmate::assertCharacter(sep, len = 1L, null.ok = FALSE, any.missing = FALSE)
   checkmate::assertCharacter(package, len = 1L, null.ok = TRUE, any.missing = FALSE)
-
-  if (! grepl(sep, bin)) stop("Separator does not appear in binary string.")
 
   # Check if model is installed and install if not:
   if (! is.null(package) && require(package, quietly = TRUE, character.only = TRUE)) {
     stop("Package '", package, "' is not installed. Please install it or contact the administrator to do this for you.")
   }
 
-  binary_str_deparse = strsplit(bin, split = sep)[[1]]
+  binary_str_deparse = substring(bin, seq(1, nchar(bin), 2), seq(2, nchar(bin), 2))
+
+  #binary_str_deparse = strsplit(bin, split = sep)[[1]]
   raw = as.raw(as.hexmode(binary_str_deparse))
   obj = unserialize(raw)
 
--- R/encodeObject.R
+++ R/encodeObject.R
@@ -5,7 +5,6 @@
 #' @param obj (arbitrary R object) Object which should be send to DataSHIELD.
 #' @param obj_name (`character(1L)`) Name of the object (default is `NULL`). If name is set to
 #'   `NULL`, then the object name passed to the function is used.
-#' @param sep (`character(1L)`) Separator used to collapse the binary elements (default is `-`).
 #' @param check_serialization (`logical(1L)`) Check if the serialized model can be deserialized
 #'   locally (default is `TRUE`).
 #' @return Character of length 1 containing the serialized object as string.
@@ -15,22 +14,24 @@
 #' bin = encodeObject(mod)
 #' substr(bin, 1, 50)
 #' @export
-encodeObject = function(obj, obj_name = NULL, sep = "-", check_serialization = TRUE) {
-  checkmate::assertCharacter(sep, len = 1L, null.ok = FALSE, any.missing = FALSE)
+encodeObject = function(obj, obj_name = NULL, check_serialization = TRUE) {
   checkmate::assertCharacter(obj_name, len = 1L, null.ok = TRUE, any.missing = FALSE)
 
   if (is.null(obj_name)) obj_name = deparse(substitute(obj))
 
   obj_binary = serialize(obj, connection = NULL)
   obj_binary_str = as.character(obj_binary)
-  obj_binary_str_collapsed = paste(obj_binary_str, collapse = sep)
+  obj_binary_str_collapsed = paste(obj_binary_str, collapse = "")
+
 
   ## Pre check if object serialization works locally:
   if (check_serialization) {
     # get object back from serialization
-    binary_str_deparse = strsplit(obj_binary_str_collapsed, split = sep)[[1]]
-    raw = as.raw(as.hexmode(binary_str_deparse))
-    obj_b = unserialize(raw)
+    obj_b = decodeBinary(obj_binary_str_collapsed)
+
+    #binary_str_deparse = strsplit(obj_binary_str_collapsed, split = sep)[[1]]
+    #raw = as.raw(as.hexmode(binary_str_deparse))
+    #obj_b = unserialize(raw)
 
     if (! all.equal(obj, obj_b)) stop("Model cannot serialized and deserialized into equal object!")
   }
@@ -41,9 +42,6 @@ encodeObject = function(obj, obj_name = NULL, sep = "-", check_serialization = T
       " MB). Uploading larger objects may take some time.")
   }
   names(obj_binary_str_collapsed) = obj_name
-  attr(obj_binary_str_collapsed, "sep") = sep
 
   return(obj_binary_str_collapsed)
 }
-
-
