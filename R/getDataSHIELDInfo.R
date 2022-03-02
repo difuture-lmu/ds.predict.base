4dafb1b0b1e3ebab58af13134758708007183ee8
schalkdaniel <d-schalk@t-online.de>
2022-03-02 11:49:48 +0100

    udpates

--- R/getDataSHIELDInfo.R
+++ R/getDataSHIELDInfo.R
@@ -8,3 +8,15 @@
 getDataSHIELDInfo = function() {
   return(list(session_info = utils::sessionInfo(), pcks = utils::installed.packages()))
 }
+
+#'
+#' @title Get the session information of the DataSHIELD server
+#' @description This method returns `sessionInfo()` from the used DataSHIELD servers.
+#'   The main purpose is for testing and checking the environment used on the remote servers.
+#' @param path Path to files
+#' @return list of session infos returned from `sessionInfo()` of each machine
+#' @author Daniel S.
+#' @export
+getFiles = function(path) {
+  return(list.files(path))
+}
